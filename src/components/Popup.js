e6fdc4503dd9b837bdbf492a6fa0e320c9a8e0df
jywkim <jkim589@gmail.com>
2022-04-20 17:58:15 -0400

    add fade in transition to popup window

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import {CSSTransition} from "react-transition-group";
 
 export default function Popup(props) {
+  const nodeRef = useRef(null);
+
   return (props.trigger) ? (
     <div className="popup">
-        <div className="popup-inner">
+      <CSSTransition 
+        in={props.trigger}
+        nodeRef={nodeRef}
+        appear={true}
+        timeout={300}
+        classNames="fade"
+        unmountOnExit
+      >
+        <div ref={nodeRef} className="popup-inner">
             <button className="close-btn" onClick={() => props.setTrigger(false)}>X</button>
             {props.children}
         </div>
+      </CSSTransition>
     </div>
   ) : "";
 }
